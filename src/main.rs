c5b582cced43dfff87c058744557ab597892b513
Davit Samvelyan <davit.samvelyan@picsart.com>
2021-01-24 23:11:24 +0400

    Flush stdout

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
     print!("Enter your name: ");
+    io::stdout().flush().unwrap();
     let mut name = String::new();
     io::stdin()
         .read_line(&mut name)
         .expect("Failed to read from standard input.");
+    let name = name.trim();
     println!("Hello {}!", name);
 }
