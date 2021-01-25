383738ac56ade2fbf61b76284d718ce0bc360061
Davit Samvelyan <davit.samvelyan@picsart.com>
2021-01-25 14:21:32 +0400

    Flush stdout

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
     print!("Enter your name: ");
+    io::stdout().flush().unwrap();
     let mut name = String::new();
     io::stdin()
         .read_line(&mut name)
